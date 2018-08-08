c86abfa0e8fb4565a4428e560aadf801e9a681e9
Colin Turner <colinturner34@gmail.com>
2018-08-07 23:14:02 -0400

    feat: implement product service to abstract away API server requests logic

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product';
+import { Http, Response } from '@angular/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProductService {
+  private productsUrl = '/api/products';
 
-  constructor() { }
+  constructor (private http: Http) {}
+
+  // get('/api/products')
+  getProducts(): Promise<void | Product[]> {
+    return this.http.get(this.productsUrl)
+      .toPromise()
+      .then(response => response.json() as Product[])
+      .catch(this.handleError);
+  }
+
+  // post('/api/products')
+  storeProduct(newProduct: Product): Promise<void | Product> {
+    return this.http.post(this.productsUrl, newProduct)
+      .toPromise()
+      .then(response => response.json() as Product)
+      .catch(this.handleError);
+  }
+
+  // delete('/api/products/:id')
+  deleteProduct(productId: String): Promise<void | String> {
+    return this.http.delete(this.productsUrl + '/' + productId)
+      .toPromise()
+      .then(response => response.json() as String)
+      .catch(this.handleError);
+  }
+
+  // put('/api/products/:id;)
+  updateProduct(product: Product): Promise<void | Product> {
+    const putUrl = this.productsUrl + '/' + product._id;
+    return this.http.put(putUrl, product)
+      .toPromise()
+      .then(response => response.json() as Product)
+      .catch(this.handleError);
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg);
+  }
+
+
+
+
 }
